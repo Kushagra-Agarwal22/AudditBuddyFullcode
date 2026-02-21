1c871f86790c84a67a8e1e1bc8c334879c578573
KUSHAGRA AGARWAL <149249227+Kushagra-Agarwal22@users.noreply.github.com>
2026-02-22 02:54:58 +0530

    Update api.js

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://audditbuddyfullcode.onrender.com';
 
 export const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
